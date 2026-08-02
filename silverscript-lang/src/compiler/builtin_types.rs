447c91e828ca4e1ad3ed2e79bcc213b09dab2265
Ori Newman <orinewman1@gmail.com>
2026-08-02 09:04:10 +0300

    Add more accurate builtin types (#183)

--- silverscript-lang/src/compiler/builtin_types.rs
+++ silverscript-lang/src/compiler/builtin_types.rs
@@ -59,18 +59,21 @@ pub(super) fn builtin_return_type(name: &str) -> Option<TypeRef> {
         | "OpCovOutputIdx" => scalar(TypeBase::Int),
         "length" => scalar(TypeBase::Int),
         "OpTxInputIsCoinbase" | "checkSig" | "checkSigFromStack" | "checkSigFromStackECDSA" => scalar(TypeBase::Bool),
-        "blake2b" | "blake2bWithKey" | "blake3" | "blake3WithKey" | "templateHash" | "sha256" => byte_array(ArrayDim::Fixed(32)),
-        "OpInputCovenantId" | "OpOutputCovenantId" => byte_array(ArrayDim::Fixed(32)),
-        "bytes"
-        | "OpTxSubnetId"
-        | "OpTxPayloadSubstr"
+        "blake2b"
+        | "blake2bWithKey"
+        | "blake3"
+        | "blake3WithKey"
+        | "templateHash"
+        | "sha256"
         | "OpOutpointTxId"
-        | "OpTxInputScriptSigSubstr"
-        | "OpTxInputSeq"
-        | "OpTxInputSpkSubstr"
-        | "OpTxOutputSpkSubstr"
-        | "OpNum2Bin"
-        | "OpChainblockSeqCommit" => byte_array(ArrayDim::Dynamic),
+        | "OpChainblockSeqCommit"
+        | "OpInputCovenantId"
+        | "OpOutputCovenantId" => byte_array(ArrayDim::Fixed(32)),
+        "OpTxSubnetId" => byte_array(ArrayDim::Fixed(20)),
+        "OpTxInputSeq" => byte_array(ArrayDim::Fixed(8)),
+        "bytes" | "OpTxPayloadSubstr" | "OpTxInputScriptSigSubstr" | "OpTxInputSpkSubstr" | "OpTxOutputSpkSubstr" | "OpNum2Bin" => {
+            byte_array(ArrayDim::Dynamic)
+        }
         _ => return None,
     })
 }
