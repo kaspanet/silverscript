4e64a917387e494e29b683dca597fbb8a706be59
Ori Newman <orinewman1@gmail.com>
2026-08-07 10:08:21 +0300

    fmt

--- silverscript-lang/src/compiler/structs.rs
+++ silverscript-lang/src/compiler/structs.rs
@@ -13,11 +13,11 @@ mod context;
 mod contract;
 mod declaration;
 mod destructure;
-mod scalar_expr;
+mod expr_lowering;
 mod layout;
+mod scalar_expr;
 mod schema;
 mod statement;
-mod expr_lowering;
 
 use assignment::lower_assignment;
 pub(crate) use context::LoweringScope;
@@ -25,18 +25,18 @@ use context::StructLowerer;
 pub(crate) use contract::{flatten_constructor_args_env, lower_structs_contract};
 use declaration::lower_variable_definition;
 use destructure::lower_struct_destructure_statement;
-use scalar_expr::lower_scalar_expr;
-pub(crate) use scalar_expr::resolve_struct_access;
+use expr_lowering::{
+    flatten_named_type, flatten_runtime_return_exprs, lower_call_args, lower_expr, lower_function_call_bindings, lower_named_expr,
+    lower_struct_array_expr,
+};
 use layout::flatten_struct_fields;
 pub use layout::flattened_struct_name;
 pub(crate) use layout::{flatten_type_leaves, flattened_struct_field_specs_for_type};
+use scalar_expr::lower_scalar_expr;
+pub(crate) use scalar_expr::resolve_struct_access;
 use schema::is_struct_like;
 pub(crate) use schema::{
     StructRegistry, build_struct_registry, ensure_known_struct_or_builtin_type, is_struct, is_struct_array, struct_array_name,
     struct_name, validate_struct_graph,
 };
 use statement::lower_statements;
-use expr_lowering::{
-    flatten_named_type, flatten_runtime_return_exprs, lower_call_args, lower_expr, lower_function_call_bindings, lower_named_expr,
-    lower_struct_array_expr,
-};
